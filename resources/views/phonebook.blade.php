94f89d099eba7b9d784170ae0ea25c428f677b8d
Mohamed <m7md.omer@gmail.com>
2018-10-02 08:24:14 +0200

    activate vue, adding nav bar componnet

--- resources/views/phonebook.blade.php
+++ resources/views/phonebook.blade.php
@@ -6,7 +6,10 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-	<div id="app"></div>
+	<div id="app">
+		<Navbar></Navbar>
+
+	</div>
 
 <script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
 </body>
